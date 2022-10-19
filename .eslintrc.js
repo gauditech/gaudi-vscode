9a109c2e2032080369514bba233e93e4ad546ad7
Ivo Kosir <ivo.kosir93@gmail.com>
2022-10-19 15:26:16 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+    "import/core-modules": ["vscode"],
+  },
+  rules: {
+    "sort-imports": [
+      "warn",
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+    "import/order": [
+      "warn",
+      {
+        "newlines-between": "always",
+        alphabetize: { order: "asc" },
+      },
+    ],
+    "import/newline-after-import": "warn",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        // ignoreRestSiblings: true,
+      },
+    ],
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+};
---
+++ src/extension.ts
@@ -0,0 +1,50 @@
+import { homedir } from "os";
+import * as path from "path";
+import { ExtensionContext } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(_context: ExtensionContext) {
+  // The server is implemented in node
+  const serverModule = path.join(homedir(), "gaudi", "gaudi", "dist", "src", "languageServer.js");
+
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "gaudi" }],
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient("gaudi", "Gaudi", serverOptions, clientOptions);
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export async function deactivate() {
+  if (!client) {
+    return undefined;
+  }
+  return await client.stop();
+}
